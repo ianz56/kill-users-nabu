b4446518e0a6176accfc0ce208849614ed9f1309
ianz56 <ianperdiansah05@gmail.com>
2026-08-03 13:19:35 +0700

    v1.0.13: Revert GooglePackageInstaller to prevent HyperOS PackageInstaller conflict bootloop

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@
 # Print banner
 ui_print "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 ui_print "  Nabu CN System & Family Link Helper"
-ui_print "  v1.0.12 by Ian Perdiansah"
+ui_print "  v1.0.13 by Ian Perdiansah"
 ui_print "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 ui_print ""
 ui_print "  Features included:"
