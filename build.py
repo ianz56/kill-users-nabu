83e0cdedb6a3bcb07e950623f2b105008de57fa3
ianz56 <ianperdiansah05@gmail.com>
2026-08-04 14:26:44 +0700

    v1.0.25: Auto-populate GSF gservices.db and enable DroidGuardGmsService for complete GSF ID compatibility

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ import sys
 import zipfile
 
 MODULE_NAME = "nabu-cn-familylink-helper"
-VERSION = "v1.0.24"
+VERSION = "v1.0.25"
 
 # Extensions that need CRLF -> LF conversion for Android
 SHELL_EXTENSIONS = {".sh"}
